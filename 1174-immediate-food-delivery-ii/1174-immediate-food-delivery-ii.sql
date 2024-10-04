50274e1dc66ebe9f588d690d7e060adc03ce9eee
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2024-10-04 21:58:07 +0900

    Time: 442 ms (55.20%), Space: 0B (100.00%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -5,19 +5,8 @@ WITH first_order AS (
         customer_pref_delivery_date,
         ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY order_date) AS rn
     FROM Delivery
-),
-order_state AS(
-    SELECT 
-        customer_id, 
-        CASE WHEN order_date = customer_pref_delivery_date THEN 'immediate' ELSE 'scheduled' END AS ord_state
-    FROM first_order
-    WHERE rn = 1
-),
-order_state_cnt AS(
-    SELECT ord_state, SUM(1) AS state_cnt
-    FROM order_state
-    GROUP BY ord_state
 )
-
-SELECT ROUND(SUM(CASE WHEN ord_state = 'immediate' THEN state_cnt ELSE 0 END)/SUM(state_cnt) ::decimal * 100, 2) AS immediate_percentage
-FROM order_state_cnt
+SELECT 
+    ROUND(SUM(CASE WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS immediate_percentage
+FROM first_order
+WHERE rn = 1;
