45f20333fe12b2e9f8add139d7fdd5a78651be39
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2024-10-04 21:33:41 +0900

    Time: 476 ms (58.07%), Space: 0B (100.00%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -1,14 +1,36 @@
-WITH all_trans AS(
-    SELECT country, SUM(1) AS all_trans_cnt, SUM(amount) AS all_trans_amount, TO_CHAR(trans_date, 'YYYY-MM') AS d
+WITH all_trans AS (
+    -- 모든 거래에 대한 집계
+    SELECT 
+        country, 
+        SUM(1) AS all_trans_cnt, 
+        SUM(amount) AS all_trans_amount, 
+        TO_CHAR(trans_date, 'YYYY-MM') AS month
     FROM Transactions
-    GROUP BY country, d
+    GROUP BY country, month
 ),
-approved AS(
-    SELECT country, SUM(1) AS approved_cnt, SUM(amount) AS approved_amount, TO_CHAR(trans_date, 'YYYY-MM') AS d
+approved AS (
+    -- 승인된 거래에 대한 집계
+    SELECT 
+        country, 
+        SUM(1) AS approved_cnt, 
+        SUM(amount) AS approved_amount, 
+        TO_CHAR(trans_date, 'YYYY-MM') AS month
     FROM Transactions
     WHERE state = 'approved'
-    GROUP BY country, d
+    GROUP BY country, month
 )
-    
-SELECT A.d AS month, A.country AS country, A.all_trans_cnt AS trans_count, COALESCE(B.approved_cnt, 0) AS approved_count, A.all_trans_amount AS trans_total_amount, COALESCE(B.approved_amount, 0) AS approved_total_amount
-FROM all_trans A LEFT JOIN approved B ON COALESCE(A.country,'COUNTRY_NULL') = COALESCE(B.country, 'COUNTRY_NULL') AND A.d = B.d;
+
+-- 모든 거래와 승인된 거래를 연결하여 최종 결과 반환
+SELECT 
+    A.month AS month, 
+    A.country AS country, 
+    A.all_trans_cnt AS trans_count, 
+    COALESCE(B.approved_cnt, 0) AS approved_count, 
+    A.all_trans_amount AS trans_total_amount, 
+    COALESCE(B.approved_amount, 0) AS approved_total_amount
+FROM 
+    all_trans A
+LEFT JOIN 
+    approved B 
+    ON COALESCE(A.country, 'COUNTRY_NULL') = COALESCE(B.country, 'COUNTRY_NULL') 
+    AND A.month = B.month;
