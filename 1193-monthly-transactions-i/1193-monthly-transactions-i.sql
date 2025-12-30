8b9db7db0bc701e492663513a33ae2890fea0586
Ahhyun, KANG <70644095+ahyun39@users.noreply.github.com>
2025-12-30 21:52:59 +0900

    Time: 562 ms (53.15%), Space: 0B (100%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -1,36 +1,3 @@
-WITH all_trans AS (
-    -- 모든 거래에 대한 집계
-    SELECT 
-        country, 
-        SUM(1) AS all_trans_cnt, 
-        SUM(amount) AS all_trans_amount, 
-        TO_CHAR(trans_date, 'YYYY-MM') AS month
-    FROM Transactions
-    GROUP BY country, month
-),
-approved AS (
-    -- 승인된 거래에 대한 집계
-    SELECT 
-        country, 
-        SUM(1) AS approved_cnt, 
-        SUM(amount) AS approved_amount, 
-        TO_CHAR(trans_date, 'YYYY-MM') AS month
-    FROM Transactions
-    WHERE state = 'approved'
-    GROUP BY country, month
-)
-
--- 모든 거래와 승인된 거래를 연결하여 최종 결과 반환
-SELECT 
-    A.month AS month, 
-    A.country AS country, 
-    A.all_trans_cnt AS trans_count, 
-    COALESCE(B.approved_cnt, 0) AS approved_count, 
-    A.all_trans_amount AS trans_total_amount, 
-    COALESCE(B.approved_amount, 0) AS approved_total_amount
-FROM 
-    all_trans A
-LEFT JOIN 
-    approved B 
-    ON COALESCE(A.country, 'NULL') = COALESCE(B.country, 'NULL') 
-    AND A.month = B.month;
+SELECT DATE_FORMAT(trans_date,'%Y-%m') month, country, COUNT(1) trans_count, COUNT(CASE WHEN state="approved" THEN 1 ELSE NULL END) approved_count, SUM(amount) trans_total_amount, SUM(CASE WHEN state="approved" THEN amount ELSE 0 END) approved_total_amount
+FROM Transactions
+GROUP BY 1, 2
